f5c1dbfc4f050e7b3e2bf9b23e4155c769956e07
ShortSoufie <112567382+ShortSoufie@users.noreply.github.com>
2024-04-03 12:23:23 +0200

    Delete cle3team12 directory

--- cle3team12/includes/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host       = "localhost";
-$user       = "root";
-$password   = "";
-$database   = "mindmate";
-
-$db = mysqli_connect($host, $user, $password, $database)
-or die("Error: " . mysqli_connect_error());;
